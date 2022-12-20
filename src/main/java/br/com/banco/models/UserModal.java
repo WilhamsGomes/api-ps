6da68a5310a30d4b88b7aa9f6da9e77d2df868dd
WilhamsGomes <wilhamsgoomes@gmail.com>
2022-12-19 23:34:11 -0300

    :sparkles: feat: Model e Repositry User

---
+++ src/main/java/br/com/banco/models/UserModal.java
@@ -0,0 +1,35 @@
+package br.com.banco.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity(name="conta")
+@Table(name="conta")
+@SequenceGenerator(name = "seq_user", sequenceName = "seq_user", allocationSize = 1, initialValue = 1)
+public class UserModal implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_user")
+    private long id_conta;
+
+    @Column(nullable = false, length = 150)
+    private String nome_responsavel;
+
+    public long getId_conta() {
+        return id_conta;
+    }
+
+    public void setId_conta(long id_conta) {
+        this.id_conta = id_conta;
+    }
+
+    public String getNome_responsavel() {
+        return nome_responsavel;
+    }
+
+    public void setNome_responsavel(String nome_responsavel) {
+        this.nome_responsavel = nome_responsavel;
+    }
+}
